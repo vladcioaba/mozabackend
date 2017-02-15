60167c5a7e7f4be3df391f7be291e2b210d3f2e6
Vlad <vlad.cioaba@gmail.com>
2017-02-15 23:08:49 +0200

    Added new scripts.

---
+++ scripts/deploy.sh
@@ -0,0 +1,2 @@
+#scp scripts/launch.sh root@82.196.12.198:/dev/
+scp bin/mozabackendserver.jar root@82.196.12.198:/home/dev/
--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,1 +1,1 @@
-java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,address="8000" -jar mozabackendserver.jar
+java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,address="8081" -jar mozabackendserver.jar
