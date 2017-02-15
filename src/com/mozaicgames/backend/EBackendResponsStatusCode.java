254bbe7be220428ba723142137ae06a0177fe0a6
Cioaba Vlad <vlad.cioaba@gmail.com>
2017-02-15 17:25:03 +0200

    Update.

--- src/com/mozaicgames/backend/EBackendResponsStatusCode.java
+++ src/com/mozaicgames/backend/EBackendResponsStatusCode.java
@@ -3,11 +3,11 @@ package com.mozaicgames.backend;
 public enum EBackendResponsStatusCode 
 {
 		STATUS_OK					(200),
-		INVALID_REQUEST				(300),
-		INVALID_DATA				(400),
-		INTERNAL_ERROR				(500),
-		RETRY_LATER					(900),
-		CLIENT_OUT_OF_DATE			(800),
+		INVALID_REQUEST				(4001),
+		INVALID_DATA				(4002),
+		INTERNAL_ERROR				(5000),
+		RETRY_LATER					(8000),
+		CLIENT_OUT_OF_DATE			(9000),
 		CLIENT_REJECTED				(1000);
 	
 		private final int mValue;
